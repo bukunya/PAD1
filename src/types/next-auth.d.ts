219ba32acb31267c5a580d40ee28ca0ad88748ce
Abdullah Afif Habiburrohman <abdullahafifhabiburrohman@mail.ugm.ac.id>
2025-12-05 00:40:53 +0700

    Feat(JWT): JWT

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,13 +1,7 @@
 import { Role } from "@prisma/client";
 import { DefaultSession, DefaultUser } from "next-auth";
 
-// This file extends the default types of Auth.js to include our custom properties.
-
 declare module "next-auth" {
-  /**
-   * The `session` object is what you will receive on the client-side.
-   * We are adding 'id' and 'role' to the default user object.
-   */
   interface Session {
     user: {
       id: string;
@@ -15,11 +9,8 @@ declare module "next-auth" {
     } & DefaultSession["user"];
   }
 
-  /**
-   * The `user` object is the shape of the user object returned from the database adapter.
-   * We need to tell TypeScript that it also has a 'role' property.
-   */
   interface User extends DefaultUser {
+    id: string;
     role: Role;
   }
 }
